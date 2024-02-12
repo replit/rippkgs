7d245397b159d3904d3d4c6b0c3ea47b867b8ecf
Colton Donnelly <colton@donn.io>
2024-02-12 17:02:34 -0500

    set flake packages

--- flake.nix
+++ flake.nix
@@ -2,6 +2,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     devshell = {
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -25,14 +30,57 @@
       "x86_64-linux"
     ];
 
-    perSystem = { inputs', pkgs, ... }: {
+    perSystem = { lib, pkgs, rust-toolchain, self', system, ... }: {
+      _module.args = {
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+
+          overlays = [
+            inputs.fenix.overlays.default
+          ];
+        };
+
+        rust-toolchain = pkgs.fenix.latest.toolchain;
+      };
+
       formatter = pkgs.alejandra;
 
-      devshells.default = {
+      devShells.default = self'.devShells.rippkgs;
+      devshells.rippkgs = {
         packages = [
-          inputs'.fenix.packages.latest.toolchain
+          pkgs.fenix.latest.toolchain
           pkgs.sqlite
         ];
+      };
+
+      packages = let
+        craneLib = inputs.crane.lib.${system}.overrideToolchain rust-toolchain;
+
+        common-args = {
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+          strict-deps = true;
+
+          buildInputs = [
+            pkgs.sqlite
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.libiconv
+          ];
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly common-args;
+      in {
+        default = self'.packages.rippkgs;
+
+        rippkgs = craneLib.buildPackage (common-args // {
+          inherit cargoArtifacts;
+          pname = "rippkgs";
+          # cargoExtraArgs = "--bin rippkgs";
+        });
+
+        rippkgs-index = craneLib.buildPackage (common-args // {
+          inherit cargoArtifacts;
+          pname = "rippkgs-index";
+        });
       };
     };
   };
