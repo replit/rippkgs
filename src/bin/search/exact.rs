b7c597540ba70dccdef8cc65d4d2086fd3084232
Colton Donnelly <colton@donn.io>
2024-02-26 16:08:37 -0500

    exact-match search (#1)
    did a bit of refactoring as well, i can split that out into a separate
    pr
    
    looks for exactly 1 match in an index db based on the attribute.

---
+++ src/bin/search/exact.rs
@@ -0,0 +1,25 @@
+use std::time::Instant;
+
+use eyre::Context;
+use rusqlite::Connection;
+
+use crate::data::Package;
+
+pub fn search(query_str: &str, db: &Connection) -> eyre::Result<Option<Package>> {
+    let start = Instant::now();
+
+    let result = db.query_row(
+        "SELECT * FROM packages WHERE attribute = ?1",
+        rusqlite::params![query_str],
+        |r| Package::try_from(r),
+    );
+
+    let elapsed = start.elapsed();
+    eprintln!("got results in {} ms", elapsed.as_millis());
+
+    match result {
+      Ok(res) => Ok(Some(res)),
+      Err(rusqlite::Error::QueryReturnedNoRows) => Ok(None),
+      Err(err) => Err(err).context("unable to execute query"),
+    }
+}
