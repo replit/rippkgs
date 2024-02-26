c6a9bc963a09cf9be74a4b89cfdae871651de433
Colton Donnelly <colton@donn.io>
2024-02-26 14:59:17 -0500

    refactor search (#3)
    Why
    ===
    
    break out to better support multiple operation types
    
    What changed
    ============
    
    - define rust struct for results (query all rows of db)
    - move `search` function to its own module called `fuzzy`
    
    Test plan
    =========
    
    _Describe what you did to test this change to a level of detail that
    allows your reviewer to test it_

--- src/bin/search/main.rs
+++ src/bin/search/main.rs
@@ -1,14 +1,11 @@
+mod data;
+mod fuzzy;
 use std::io::stdout;
 use std::path::PathBuf;
-use std::time::Instant;
 
 use clap::{Parser, ValueEnum};
 use eyre::Context;
 use eyre::Result;
-use fuzzy_matcher::skim::SkimMatcherV2;
-use fuzzy_matcher::FuzzyMatcher;
-use rusqlite::functions::Context as FunctionContext;
-use rusqlite::functions::FunctionFlags;
 use rusqlite::OpenFlags;
 
 #[derive(Debug, Parser)]
@@ -41,76 +38,10 @@ fn main() -> Result<()> {
     )
     .context("unable to read index")?;
 
-    conn.create_scalar_function(
-        "fuzzy_score",
-        2,
-        FunctionFlags::SQLITE_UTF8,
-        scalar_fuzzy_score,
-    )
-    .context("unable to install `fuzzy_score` function")?;
+    let results = fuzzy::search(opts.query.as_str(), &conn, opts.num_results)
+        .context("error getting results")?;
 
-    let mut query = conn
-        .prepare(
-            r#"
-SELECT *
-FROM packages
-ORDER BY fuzzy_score(name, ?1) DESC
-LIMIT ?2
-            "#,
-        )
-        .context("unable to prepare search query")?;
-
-    let start = Instant::now();
-
-    let mut results = query
-        .query(rusqlite::params![opts.query, opts.num_results])
-        .context("unable to execute query")?;
-
-    let mut result_json_object = Vec::<serde_json::Value>::with_capacity(opts.num_results as _);
-
-    loop {
-        let row = results.next().context("error collecting query results")?;
-
-        let Some(row) = row else {
-            break;
-        };
-
-        let attribute: String = row.get("attribute").context("error reading column")?;
-        // let store_path: String = row.get("store_path").context("error reading column")?;
-        let name: Option<String> = row.get("name").context("error reading column")?;
-        let version: Option<String> = row.get("version").context("error reading column")?;
-        let description: Option<String> = row.get("description").context("error reading column")?;
-        let homepage: Option<String> = row.get("homepage").context("error reading column")?;
-        let long_description: Option<String> = row
-            .get("long_description")
-            .context("error reading column")?;
-
-        result_json_object.push(serde_json::json!({
-          "attribute": attribute,
-          // "storePath": store_path,
-          "name": name,
-          "version": version,
-          "description": description,
-          "homepage": homepage,
-          "longDescription": long_description,
-        }));
-    }
-
-    serde_json::to_writer(stdout(), &result_json_object).context("error printing result")?;
-
-    let elapsed = start.elapsed();
-    eprintln!("finished in {} ms", elapsed.as_millis());
+    serde_json::to_writer(stdout(), &results).context("error printing results")?;
 
     Ok(())
-}
-
-fn scalar_fuzzy_score(ctx: &FunctionContext) -> rusqlite::Result<i64> {
-    lazy_static::lazy_static! {
-      static ref MATCHER: SkimMatcherV2 = SkimMatcherV2::default();
-    }
-
-    let choice = ctx.get::<String>(0)?;
-    let pattern = ctx.get::<String>(1)?;
-
-    Ok(MATCHER.fuzzy_match(&choice, &pattern).unwrap_or(0))
 }
